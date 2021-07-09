ff2e48f547b594df284066f69267a5df7fdcf24e
Dubpace <87080525+Dubpace@users.noreply.github.com>
2021-07-09 15:14:43 -0400

    add alive status variable

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-//Declare variables for use in Kanye message script
+//Declare variables for use in inspirational message script
 //Set name of user for greeting
 const username = 'Will';
+//Determine if celeb is alive or dead
+const alive = Math.random() < 0.5;
 //Store quote possibilites as an array
 const kanyeQuotes = [
     `If you have the opportunity to play this game of life you need to appreciate every moment. A lot of people don’t appreciate the moment until it’s passed.`,
@@ -14,11 +16,10 @@ const kanyeQuotes = [
     `The most genius thing about the way I create is to create with other geniuses.`,
     `It's not 'can'; it's 'will.' You have to will things into fruition.`,
 ];
-const randomIndex = 0;
 
 const greeting = () => {
     console.log(`Hello, ${username}! Here is your Kanye quote:`);
 }
 //Greet user to the program
 greeting();
-console.log(`Testing index 2: ${kanyeQuotes[9]}`);
+// console.log(alive);
