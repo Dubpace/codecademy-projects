c733c89088aed46d61d5ee91a3f729d55258b2fd
Dubpace <87080525+Dubpace@users.noreply.github.com>
2021-07-09 14:46:00 -0400

    add initial kanye quotes

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
 const username = 'Will';
 //Store quote possibilites as an array
 const kanyeQuotes = [
-    
+    `If you have the opportunity to play this game of life you need to appreciate every moment. A lot of people don’t appreciate the moment until it’s passed.`,
+    `Love your haters, they’re your biggest fans.`,
+    `I don’t care about what people think, because people don’t think.`,
 ];
 const randomIndex = 0;
 
@@ -12,3 +14,4 @@ const greeting = () => {
 }
 //Greet user to the program
 greeting();
+console.log(`Testing index 2: ${kanyeQuotes[2]}`);
