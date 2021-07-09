cccdb66bd3408906cc3f0ed48dda5d60a398ef20
Dubpace <87080525+Dubpace@users.noreply.github.com>
2021-07-09 15:25:09 -0400

    reduce kanye quotes, add spielberg

--- main.js
+++ main.js
@@ -4,17 +4,20 @@ const username = 'Will';
 //Determine if celeb is alive or dead
 const alive = Math.random() < 0.5;
 //Store quote possibilites as an array
-const kanyeQuotes = [
-    `If you have the opportunity to play this game of life you need to appreciate every moment. A lot of people don’t appreciate the moment until it’s passed.`,
+const kanye = [
     `Love your haters, they’re your biggest fans.`,
-    `I don’t care about what people think, because people don’t think.`,
     `For me, giving up is harder than trying.`,
     `I was never really good at anything except for the ability to learn.`,
     `Would you believe in what you believe in if you were the only one who believed it?`,
-    `We all self-conscious. I’m just the first to admit it.`,
     `Art is to be free. Design is to fix.`,
     `The most genius thing about the way I create is to create with other geniuses.`,
-    `It's not 'can'; it's 'will.' You have to will things into fruition.`,
+];
+const spielberg = [
+    `All of us every single year, we're a different person. I don't think we're the same person all our lives.`,
+    `Why pay a dollar for a bookmark? Why not use the dollar for a bookmark?`,
+    `I've discovered I've got this preoccupation with ordinary people pursued by large forces.`,
+    `The delicate balance of mentoring someone is not creating them in your own image, but giving them the opportunity to create themselves.`,
+    `You shouldn't dream your film, you should make it!`,
 ];
 
 const greeting = () => {
@@ -22,4 +25,4 @@ const greeting = () => {
 }
 //Greet user to the program
 greeting();
-// console.log(alive);
+console.log(alive);
