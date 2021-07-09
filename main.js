dc53c75b3c58490f14bb6884cf851279314810f8
Dubpace <87080525+Dubpace@users.noreply.github.com>
2021-07-09 14:37:48 -0400

    refactor greeting to func call

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
+//Set name of current user
 const username = 'Will';
-console.log(`Hello, ${username}! Here is your Kanye quote:`);
+
+const greeting = () => {
+    console.log(`Hello, ${username}! Here is your Kanye quote:`);
+}
+//Greet user to the program
+greeting();
