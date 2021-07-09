a06614b4f5e978763c21e2f3c9a0b083c4a55dd7
Dubpace <87080525+Dubpace@users.noreply.github.com>
2021-07-09 14:55:04 -0400

    add remaining quotes to array

--- main.js
+++ main.js
@@ -6,6 +6,13 @@ const kanyeQuotes = [
     `If you have the opportunity to play this game of life you need to appreciate every moment. A lot of people don’t appreciate the moment until it’s passed.`,
     `Love your haters, they’re your biggest fans.`,
     `I don’t care about what people think, because people don’t think.`,
+    `For me, giving up is harder than trying.`,
+    `I was never really good at anything except for the ability to learn.`,
+    `Would you believe in what you believe in if you were the only one who believed it?`,
+    `We all self-conscious. I’m just the first to admit it.`,
+    `Art is to be free. Design is to fix.`,
+    `The most genius thing about the way I create is to create with other geniuses.`,
+    `It's not 'can'; it's 'will.' You have to will things into fruition.`,
 ];
 const randomIndex = 0;
 
@@ -14,4 +21,4 @@ const greeting = () => {
 }
 //Greet user to the program
 greeting();
-console.log(`Testing index 2: ${kanyeQuotes[2]}`);
+console.log(`Testing index 2: ${kanyeQuotes[9]}`);
