ff257f713cb3d74d847ed16c4fb803a99430d128
Dubpace <87080525+Dubpace@users.noreply.github.com>
2021-07-09 14:40:52 -0400

    set initial variable declarations

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-//Set name of current user
+//Declare variables for use in Kanye message script
+//Set name of user for greeting
 const username = 'Will';
+//Store quote possibilites as an array
+const kanyeQuotes = [
+    
+];
+const randomIndex = 0;
 
 const greeting = () => {
     console.log(`Hello, ${username}! Here is your Kanye quote:`);
