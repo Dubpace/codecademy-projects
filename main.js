e5cb5420e2d0f22cf5fa9c0091c85092f72811ad
Dubpace <87080525+Dubpace@users.noreply.github.com>
2021-07-09 15:35:01 -0400

    add banksy quotes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const username = 'Will';
 //Determine if celeb is alive or dead
 const alive = Math.random() < 0.5;
 //Store quote possibilites as an array
-const kanye = [
+const kanyeQuotes = [
     `Love your haters, they’re your biggest fans.`,
     `For me, giving up is harder than trying.`,
     `I was never really good at anything except for the ability to learn.`,
@@ -12,12 +12,19 @@ const kanye = [
     `Art is to be free. Design is to fix.`,
     `The most genius thing about the way I create is to create with other geniuses.`,
 ];
-const spielberg = [
+const spielbergQuotes = [
     `All of us every single year, we're a different person. I don't think we're the same person all our lives.`,
     `Why pay a dollar for a bookmark? Why not use the dollar for a bookmark?`,
     `I've discovered I've got this preoccupation with ordinary people pursued by large forces.`,
     `The delicate balance of mentoring someone is not creating them in your own image, but giving them the opportunity to create themselves.`,
     `You shouldn't dream your film, you should make it!`,
+];
+const banksyQuotes = [
+    `Art should comfort the disturbed and disturb the comfortable.`,
+    `Think outside the box, collapse the box, and take a fucking sharp knife to it.`,
+    `There's nothing more dangerous than someone who wants to make the world a better place.`,
+    `All artists are willing to suffer for their work. But why are so few prepared to learn to draw?`,
+    `People who enjoy waving flags don't deserve to have one`,
 ];
 
 const greeting = () => {
@@ -25,4 +32,4 @@ const greeting = () => {
 }
 //Greet user to the program
 greeting();
-console.log(alive);
+// console.log(alive);
